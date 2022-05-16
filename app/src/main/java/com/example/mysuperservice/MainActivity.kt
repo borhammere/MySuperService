9a015908fd9dcf5091d9c637722c4e4d9d916392
borhammere <borhammere@gmail.com>
2022-05-16 20:29:29 +0300

    Toast without service

--- app/src/main/java/com/example/mysuperservice/MainActivity.kt
+++ app/src/main/java/com/example/mysuperservice/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mysuperservice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.example.mysuperservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.toastServiceButton.setOnClickListener {
+            Toast.makeText(this, (it as Button).text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
